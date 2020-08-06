7a575a712d22f5bfccd86e16a04cc91d2c8e13a4
Richard Bidin <richardbidin@outlook.com>
2020-08-06 17:44:24 -0300

    Added gatsby config files

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: ['**/styles.js'],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      options: {
+        fonts: [
+          {
+            family: 'Merriweather',
+            variants: ['400', '700'],
+          },
+        ],
+      },
+    },
+  ],
+};
