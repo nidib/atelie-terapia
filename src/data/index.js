17706c22755893a6e840f5ffbefc3c902a7f1a2d
Richard Bidin <richardbidin@outlook.com>
2020-08-07 14:03:34 -0300

    Created sitemap as an external asset

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+export const siteMap = [
+  {
+    id: 0,
+    name: 'Home',
+    href: '/',
+  },
+  {
+    id: 1,
+    name: 'Especialidades',
+    href: '/especialidades',
+  },
+  {
+    id: 2,
+    name: 'Sobre',
+    href: '/sobre',
+  },
+  {
+    id: 3,
+    name: 'Contato',
+    href: '/contato',
+  },
+];
+
+export const services = [
+  {
+    id: 0,
+  },
+];
