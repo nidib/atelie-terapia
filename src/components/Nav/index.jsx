ab9dd6967763bd23764227dcfecdd530f565ec72
Richard Bidin <richardbidin@outlook.com>
2020-08-07 14:09:46 -0300

    Moved active anchor to global && refactored nav with imported sitemap

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
+import { siteMap } from '../../data';
+
 import './styles.scss';
 
 const Nav = ({ navActive }) => {
   return (
     <nav className={navActive ? 'active' : null}>
       <ul>
-        <li>
-          <Link to="/" activeClassName="active">Home</Link>
-        </li>
-        <li>
-          <Link to="/especialidades" activeClassName="active">Especialidades</Link>
-        </li>
-        <li>
-          <Link to="/sobre" activeClassName="active">Sobre</Link>
-        </li>
-        <li>
-          <Link to="/contato" activeClassName="active">Contato</Link>
-        </li>
+        {
+          siteMap.map((link) => {
+            return (
+              <li key={link.id}>
+                <Link to={link.href} activeClassName="active">{link.name}</Link>
+              </li>
+            );
+          })
+        }
         <li>
           <a href="http://atelieterapia.com.br/">Arteterapia</a>
         </li>
         <li>
-          <a href="#">Loja (Em breve)</a>
+          <a href="#" className="disabled">Loja (Em breve)</a>
         </li>
       </ul>
     </nav>
